97b8e4f05e07d2ff2fbdde50d8094780b1625fa8
Victor Henrick <victorhenrick37@gmail.com>
2025-07-30 15:13:26 -0300

    commit medcard

---
+++ Pratica/src/Pratica.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+
+public class Pratica {
+
+  
+    public static void main(String[] args) {
+          
+
+  JOptionPane.showMessageDialog(null,"Oi , desde de ja agradeco por participar da nossa pesquisa " + " somos do grupo Mix mateus, nos der uma nota de 0 a 10 sobre a nossa estrutura .", "Supermercado Mix Matheus",JOptionPane.INFORMATION_MESSAGE);
+        
+        
+        
+    
+     Tela t = new Tela();
+ 
+    t.setVisible(true);
+    
+    }
+    }
+    
+
---
+++ Pratica/src/get_set.java
@@ -0,0 +1,41 @@
+
+
+public class get_set {
+    int nota, idade;
+    String nome, resultado,mensagem;
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+  
+    public int getNota() {
+        return nota;
+    }
+
+    public void setNota(int nota) {
+        this.nota = nota;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+
+}
